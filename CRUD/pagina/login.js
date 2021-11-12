2c447699575fc6a3b8f7befd6331593b6bc34532
yvett1985 <yarymi04@gmail.com>
2021-11-11 19:46:04 -0500

    Actividad

--- CRUD/pagina/login.js
+++ CRUD/pagina/login.js
@@ -21,11 +21,13 @@ var iniciarsesion = function(){
   console.log(password)
 
   document.getElementById('inputEmail').classList.remove("borderojo")
+  document.getElementById('inputPassword').classList.remove('borderojo')
 
-  if(email == null || email == '' || email == undefined){
+  if(email == null || email == "" || email == undefined){
       //alert('Email es obligatoorio')
       mensajeria('Email es obligatorio', 'danger')
       document.getElementById('inputEmail').classList.add("borderojo")
+      document.getElementById('inputEmail').focus()
       //mensajeria('Email es obligatorio', 'warning')
       //mensajeria('Email es obligatorio', 'success')
       return false; //No deja seguir al siguiente CAMPO hasta que no se llene 
@@ -33,17 +35,30 @@ var iniciarsesion = function(){
 
   if(password == null || password == '' || password == undefined){
       mensajeria('Password es obligatoorio', 'warning')
+      document.getElementById('inputPassword').classList.add('borderojo')
+      document.getElementById('inputPassword').focus()
       return false;
   }
 
      
   var emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
   //Se muestra un texto a modo de ejemplo, luego va a ser un icono
+  
   if (emailRegex.test(email)) {
-    alert('Correo Valido')
-    
-  } else {
+    //alert('Correo Valido')
+    mensajeria('correo valido','success')
+  }else{
     mensajeria('Correo Invalido','success')
     return false
   }
+
+  var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}[^'\s]/;
+
+  if (passwordRegex.test(password)){
+    mensajeria('inicio exitoso','success')
+  }else{
+  mensajeria('contraseña debe tener min 8 max 15 caracteres Al menos una letra mayúscula y una minuscula Al menos un dígito No espacios en blanco Al menos un caracter especial','danger')
+  return false
+  }
+
 } 
